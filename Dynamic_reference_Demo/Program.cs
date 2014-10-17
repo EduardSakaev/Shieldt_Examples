2f28605b2d9e454b54f3740854a72fdeae2e6ab9
eduarsakaev <EduardSakaev@gmail.com>
2014-10-17 11:49:45 +0300

    New classes, new keawords, DLLs

---
+++ Dynamic_reference_Demo/Program.cs
@@ -0,0 +1,44 @@
+﻿// Использовать тип dynamic вместе с рефлексией. 
+using System; 
+using System.Reflection; 
+class DynRefDemo
+{ 
+    static void Main() 
+    { 
+        Assembly asm = Assembly.LoadFrom("MyClass.dll") ; 
+        Type[] all = asm.GetTypes() ; 
+        // Найти класс DivBy. 
+        int i; 
+        for(i =0; i < all.Length; i++) 
+        if(all[i].Name == "DivBy") break; 
+        if(i == all.Length) { 
+        Console.WriteLine("Класс DivBy не найден в сборке."); 
+        return; 
+        } 
+        Type t = all[i]; 
+        //А теперь найти используемый по умолчанию конструктор. 
+        ConstructorInfo[] ci = t.GetConstructors(); 
+        int j ; 
+        for(j =0; j < ci.Length; j++) 
+        if(ci[j].GetParameters().Length == 0) break; 
+        if(j == ci.Length) 
+        { 
+        Console.WriteLine("Используемый по умолчанию конструктор не найден."); 
+        return; 
+        }  
+        // Создать объект класса DivBy динамически, 
+        dynamic obj = ci[j].Invoke(null); 
+        // Далее вызвать по имени методы для переменной obj. Это вполне допустимо, 
+        // поскольку переменная obj относится к типу dynamic, а вызовы методов 
+        // проверяются на соответствие типов во время выполнения, а не компиляции, 
+        if(obj.IsDivBy(5, 3)) 
+            Console.WriteLine("5 делится нацело на 3."); 
+        else 
+            Console.WriteLine("5 HE делится нацело на 3.");
+
+        if(obj.IsEven(9)) 
+            Console.WriteLine("9 четное число."); 
+        else 
+            Console.WriteLine("9 HE четное число."); 
+    }
+}
---
+++ Readonly/Program.cs
@@ -0,0 +1,30 @@
+﻿// Продемонстрировать применение поля с ключевым словом readonly. 
+using System;
+class MyClass
+{
+    public readonly int SIZE;
+    public MyClass(int x) { SIZE = x; }
+}
+
+class DemoReadOnly 
+{ 
+    static void Main() 
+    {
+        MyClass myclassobj = new MyClass(10);
+        int[] source = new int[myclassobj.SIZE];
+        int[] target = new int[myclassobj.SIZE]; 
+        // Присвоить ряд значений элементам массива source, 
+        for (int i = 0; i < myclassobj.SIZE; i++) 
+        source[i] = i; 
+        foreach(int i in source) 
+        Console.Write(i + " "); 
+        Console.WriteLine(); 
+        // Перенести обращенную копию массива source в массив target, 
+        for (int i = myclassobj.SIZE - 1, j = 0; i > 0; i--, j++) 
+        target[j] = source[i]; 
+        foreach(int i in target) 
+        Console.Write(i + " "); 
+        Console.WriteLine();
+        //myclassobj.SIZE = 100; // Ошибка!!! Не подлежит изменению! 
+    } 
+} 
