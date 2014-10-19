bf0e8af03caa71856778417927a1d0e95964111e
eduardsakaev <eduardsakaev@gmail.com>
2014-10-19 22:43:01 +0300

    Added Multithreading paragraph

---
+++ Multitheading_MyThread/Program.cs
@@ -0,0 +1,48 @@
+﻿// Создать поток исполнения. 
+using System; 
+using System.Threading; 
+
+class MyThread
+{ 
+    public int Count; 
+    string thrdName; 
+    public MyThread(string name)
+    { 
+        Count = 0; 
+        thrdName = name; 
+    } 
+    // Точка входа в поток, 
+    public void Run() 
+    { 
+        Console.WriteLine(thrdName + " начат."); 
+        do 
+        { 
+            Thread.Sleep(100); 
+            Console.WriteLine("В потоке " + thrdName + ", Count = " + Count); 
+            Count++; 
+        } while(Count < 10) ; 
+        Console.WriteLine(thrdName + " завершен."); 
+    } 
+}
+class MultiThread
+{
+    static void Main()
+    { 
+        Console.WriteLine("Основной поток начат."); 
+        // Сначала сконструировать объект типа MyThread. 
+        MyThread mt = new MyThread("Потомок #1"); 
+        // Далее сконструировать поток из этого объекта. 
+        Thread newThrd = new Thread(mt.Run); 
+        // И наконец, начать выполнение потока. 
+        newThrd.Start();
+        int Count = 0;
+        do 
+        { 
+            Thread.Sleep(100);
+            Console.WriteLine("В основном потоке " + "Count = " + Count);
+            Count++;
+        } while (mt.Count != 10); 
+        Console.WriteLine("Основной поток завершен."); 
+    }
+} 
+
---
+++ Multithreading_Few_Threads/Program.cs
@@ -0,0 +1,45 @@
+﻿// Создать несколько потоков исполнения. 
+using System;
+using System.Threading;
+class MyThread
+{
+    public int Count;
+    public Thread Thrd;
+    public MyThread(string name)
+    {
+        Count = 0;
+        Thrd = new Thread(this.Run);
+        Thrd.Name = name;
+        Thrd.Start();
+    }
+    // Точка входа в поток, 
+    void Run() 
+    { 
+        Console.WriteLine(Thrd.Name + " начат."); 
+        do 
+        { 
+            Thread.Sleep(100); 
+            Console.WriteLine("В потоке " + Thrd.Name + ", Count = " + Count); 
+            Count++; 
+        } while(Count < 10); 
+        Console.WriteLine(Thrd.Name + " завершен."); 
+    }
+}
+class MoreThreads
+{
+    static void Main() { 
+    Console.WriteLine("Основной поток начат."); 
+    // Сконструировать три потока. 
+    MyThread mtl = new MyThread("Потомок #1"); 
+    MyThread mt2 = new MyThread("Потомок #2"); 
+    MyThread mt3 = new MyThread("Потомок #3"); 
+    do 
+    { 
+        Console.Write ("."); 
+        Thread.Sleep (100); 
+    } while (mtl.Count < 10 || 
+            mt2.Count < 10 || 
+            mt3.Count < 10); 
+    Console.WriteLine("Основной поток завершен."); 
+    }
+}
